31150ab8a8c88849e0c6e5de674f819dd8d97561
AaronWard <aaronlaptop12@hotmail.com>
2018-04-04 02:00:54 +0100

    nearly working

--- train.py
+++ train.py
@@ -17,7 +17,6 @@ ROOT_PATH = os.getcwd()
 TRAINING_DIR = os.getcwd() + '/data/training'
 
 ####################################### DATA PREPROCESSING - Labeling ################################################
-
 # list = os.listdir(TRAINING_DIR)
 # # Add label names to list of labels
 # for folders in list:
@@ -48,16 +47,18 @@ def load_data(TRAINING_DIR):
         file_names = [os.path.join(label_directory, f) 
                         for f in os.listdir(label_directory) 
                           if f.endswith(".png")]
-
         #Traverse through each file, add the image data
         # and label to the 2 lists
         for f in file_names:
             images.append(skimage.data.imread(f))
-            labels.append(d)
+            labels.append(int(d))
 
     return images, labels
 
 images, labels = load_data(TRAINING_DIR)
+
+images = np.array(images)
+labels = np.array(labels)
 
 ####################################### DATA VISUALISATION #######################################
 ''''
@@ -70,7 +71,6 @@ copy and past the code from visualization.py into
 '''
 import matplotlib.pyplot as plt 
 
-
 ####################################### DATA PREPROCESSING - Imaging #######################################
 '''
 This cell is for image downsampling and transformation
@@ -79,43 +79,42 @@ This is on the fly to resize the images to a 50x50 size
 from skimage import transform, exposure
 from skimage.color import rgb2gray
 
-print('equalizing exposure...')
-images = [exposure.equalize_adapthist(image, clip_limit=0.0001)for image in images]
-
 print('Down scaling images...')
-image50 = [transform.resize(image, (50, 50)) for image in images]
+images = [transform.resize(image, (50, 50)) for image in images]
 
-image50 = np.array(image50)
-# image50 = rgb2gray(image50)
+# print('equalizing exposure...')
+# images = [exposure.equalize_adapthist(image, clip_limit=0.0001)for image in images50]
 
-print('plotting...')
-random = [295, 3098, 997, 4999]
-for i in range(len(random)):
-    plt.subplot(1, 4, i+1)
-    plt.axis('off')
-    plt.imshow(images[random[i]])
-    plt.subplots_adjust(wspace=0.1)
 
-plt.savefig('exp.jpg')
+# Plot the down sized / augmented image
+# random = [295, 3098, 997, 4999]
+# for i in range(len(random)):
+#     plt.subplot(1, 4, i+1)
+#     plt.axis('off')
+#     plt.imshow(images[random[i]])
+#     plt.subplots_adjust(wspace=0.1)
+# plt.savefig('exp.jpg')
 
 
 
 #################################### VARIABLE INITIATATION #################################################
 
 # Define initial variables
-batch_size = 100
+batch_size = 128
 num_class = 6
 num_epochs = 25
 
-# Define placeholders
-x = tf.placeholder('float', [None, 2500]) # 50 x 50 = 2500
-y = tf.placeholder('float')
+# Initialize placeholders 
+
+# x = tf.placeholder('float', [5582, 50, 50])
+x = tf.placeholder(dtype = tf.float32, shape = [None, 50, 50])
+y = tf.placeholder(dtype = tf.int32, shape = [None])
 
 #define variables for dropout
 keep_rate = .8
 keep_prop = tf.placeholder(tf.float32)
 
-######################################## HELPER FUNCTIONS #################################################
+# ######################################## HELPER FUNCTIONS #################################################
 
 # Extract features
 def conv2d(x, W):
@@ -125,7 +124,7 @@ def conv2d(x, W):
 def maxpool2d(x):
   return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME') #pool 2 pixels at a time
 
-######################################## NETWORK DEFINITION ################################################
+# # ######################################## NETWORK DEFINITION ################################################
 
 
 # Define the weights and biases as dictionaries and
@@ -168,30 +167,63 @@ def convolutional_network(x):
     return output
 
 
-####################################### TENSORFLOW SESSION ###################################################
+# ####################################### TENSORFLOW SESSION ###################################################
+
+# # Flatten the input data
+# images_flat = tf.contrib.layers.flatten(x)
+# # Fully connected layer 
+# logits = tf.contrib.layers.fully_connected(images_flat, 6, tf.nn.relu)
+# # Define a loss function
+# loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = logits))
+# # Define an optimizer 
+# train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
+# # Convert logits to label indexes
+# correct_pred = tf.argmax(logits, 1)
+# # Define an accuracy metric
+# accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+# print("images_flat: ", images_flat)
+# print("logits: ", logits)
+# print("loss: ", loss)
+# print("predicted_labels: ", correct_pred)
+
+
+# tf.set_random_seed(1234)
+# sess = tf.Session()
+
+# sess.run(tf.global_variables_initializer())
+
+# for i in range(201):
+#         print('EPOCH', i)
+#         _, accuracy_val = sess.run([train_op, accuracy], feed_dict={x: images, y: labels})
+#         if i % 10 == 0:
+#             print("Loss: ", loss)
+#         print('DONE WITH EPOCH')
+
+# sess.close()
+
+
 
 def train_network(x):
     pred = convolutional_network(x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels= y))
-    optimzer = tf.train.AdamOptimizer().minimize(cost)
+    # cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels= y))
+    loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = y, logits = pred))
+    train_op = tf.train.AdamOptimizer(learning_rate=0.001).minimize(loss)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer()) # Initialize all the variables
 
+        print("RUNNING SESSION...")
+
         for epoch in range(num_epochs):
-            loss = 0
+            for _ in range(1):
+                _, loss_value = sess.run([train_op, loss], feed_dict={x: images, y: labels})
+            print('Epoch : ', epoch+1, ' of ', num_epochs, ' - Loss: ', loss_value)
 
-            # Loop through 
-            #
-            #
-            #
-            # 
-            # print('Epoch : ', epoch+1, ' of ', num_epochs, ' - Loss: ', loss)
+        correct = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+        acc = tf.reduce_mean(tf.cast(correct, 'float'))
+        print('Accuracy:', acc)
 
-        # correct = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-        # acc = tf.reduce_mean(tf.cast(correct, 'float'))
-        # print('Accuracy:', acc)
+# ###########################################################################################################
 
-###########################################################################################################
-
-# train_network(x)
+train_network(x)
