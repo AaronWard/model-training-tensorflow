507d4d159f484fdb21d25c8becb9a7dc26c01678
AaronWard <aaronlaptop12@hotmail.com>
2018-04-22 11:23:46 +0100

    Completed

--- train.py
+++ train.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[1]:
+
 '''
 This is the model that will be used to train the deep convolutional neaural network.
 
@@ -6,20 +11,42 @@ This is the model that will be used to train the deep convolutional neaural netw
 import tensorflow as tf
 import os, os.path
 import pandas as pd
+import time
 import numpy as np
 from numpy import ndarray
 import skimage
 from skimage import data, io, filters
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+# sets the backend of matplotlib to the 'inline' backend
+get_ipython().magic("matplotlib inline")
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  #Suppress AVX Warnings
-
-#For floydhub
-# TRAINING_DIR = '/data/training'
-# MODEL_PATH = '/output/trained_model.ckpt'
+print('imported')
 
 
-TRAINING_DIR = os.getcwd() + '/data/training'
-MODEL_PATH = os.getcwd() + '/output/trained_model.ckpt'
+# In[2]:
+
+'''
+Basic import directories and parameters initialized
+
+Remember to use os.getcwd() initializing partha on local 
+machine
+
+'''
+TRAINING_DIR = '/data/training'
+TESTING_DIR = '/data/testing'
+MODEL_PATH = '/output/trained_model.ckpt'
+SAVE = '/output/'
+
+# Define initial variables
+batch_size = 100
+num_class = 6
+num_epochs = 100
+
+
+# In[3]:
+
 ####################################### DATA PREPROCESSING - Labeling ################################################
 '''
 This function traverses throwe ach directory of training images
@@ -34,6 +61,9 @@ def load_data(TRAINING_DIR):
     labels = []
     directories = [d for d in os.listdir(TRAINING_DIR) 
                 if os.path.isdir(os.path.join(TRAINING_DIR, d))]
+    # Need to sort these because
+    # floyd hum jumbled up the order
+    directories = sorted(directories, key=int)
 
     # Traverse through each directory and make a list
     # of files names if they end in the PNG format
@@ -52,54 +82,109 @@ def load_data(TRAINING_DIR):
 
 images, labels = load_data(TRAINING_DIR)
 
-images = np.array(images)
-labels = np.array(labels)
+print('Data impored...')
 
-#One hot encoding
-labels = pd.get_dummies(labels)
 
-####################################### DATA VISUALISATION #######################################
-''''
-This cell is for displaying data visualy using Matplotlib
-This does not have any bearing on the model so can be commented out when
-not being used.
-
-copy and past the code from visualization.py into
+# In[4]:
 
 '''
-# import matplotlib.pyplot as plt 
+Shuffle the entire dataset and labels
+
+'''
+from sklearn.utils import shuffle
+images, labels = shuffle(images, labels)
+
+
+# In[5]:
+
+'''
+This cell is  for converting the lists to numpy arrays 
+
+'''
+num_images = len(images)
+images = np.array(images, object)
+labels = np.array(labels, dtype = np.int32)
+
+_labels = np.zeros((num_images, num_class))
+_labels[np.arange(num_images), labels] = 1.0
+labels = _labels
+
+print(labels[1])
+print(labels[2])
+print(labels[3])
+print(labels[4])
+
+
+# In[6]:
+
+####################################### DATA PREPROCESSING - Labeling ################################################
+'''
+import test data and labels
+'''
+def load_test_data(TESTING_DIR):
+    test_images = []
+    test_labels = []
+    directories = [d for d in os.listdir(TESTING_DIR) 
+                if os.path.isdir(os.path.join(TESTING_DIR, d))]
+    # Need to sort these because
+    # floyd hum jumbled up the order
+    directories = sorted(directories, key=int)
+
+    # Traverse through each directory and make a list
+    # of files names if they end in the PNG format
+    for d in directories:
+        label_directory = os.path.join(TESTING_DIR, d)
+        file_names = [os.path.join(label_directory, f) 
+                        for f in os.listdir(label_directory) 
+                          if f.endswith(".png")]
+        #Traverse through each file, add the image data
+        # and label to the 2 lists
+        for f in file_names:
+            test_images.append(skimage.data.imread(f))
+            test_labels.append(int(d))
+
+    return test_images, test_labels
+
+test_images, test_labels = load_data(TESTING_DIR)
+
+test_images = np.array(test_images, object)
+test_labels = np.array(test_labels, object)
+
+# Convert labels into a one hot vector 
+test_labels = pd.get_dummies(test_labels)
+print('imported...')
+
+
+# In[7]:
 
 ####################################### DATA PREPROCESSING - Imaging #######################################
 '''
 This cell is for image downsampling and transformation
 This is on the fly to resize the images to a 50x50 size
+
 '''
 from skimage import transform, exposure
-# from skimage.color import rgb2gray
 
-print('Down scaling images...')
+print('Down scaling train images...')
 images = [transform.resize(image, (50, 50)) for image in images]
 
-# print('equalizing exposure...')
-# images = [exposure.equalize_adapthist(image, clip_limit=0.0001)for image in images50]
+print('Down scaling test images...')
+test_images = [transform.resize(test_image, (50, 50)) for test_image in test_images]
 
+print('Images Downscaled...')
+
+
+# In[8]:
 
 '''
 This cell is for initializing variables for the tensorflow session and 
 placeholders for holding the data.
 
 '''
-
-# Define initial variables
-batch_size = 100
-num_class = 6
-num_epochs = 100
-
 # Initialize placeholders 
 x = tf.placeholder(dtype = tf.float32, shape = [None, 50, 50], name='X_placeholder')
-
-y = tf.placeholder(dtype = tf.int32, shape= [batch_size, num_class],name="Y_placeholder")
-
+y = tf.placeholder(dtype = tf.int32, shape= [None, num_class], name="Y_placeholder")
+is_training = tf.placeholder( dtype = tf.bool, shape = (), name = "is_training" )
 
 #define variables for dropout
 keep_rate = .8
@@ -107,130 +192,76 @@ keep_prop = tf.placeholder(tf.float32)
 print('initialized')
 
 
-# ######################################## HELPER FUNCTIONS #################################################
+# In[3]:
+
+######################################### NETWORK STRUCTURE #################################################
+'''
+This cell is for defining the stucture of the neural network.
+The network has 11 convolutional layers and 2 fully connected layers
 
 '''
-This cell just contains helper functions for defining convolution
-and maxpooling layers
+import tensorflow.contrib.slim as slim
 
-'''
-# Extract features
-def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME') #move one pixel at s time
+def convolutional_network(x, is_training):
+    conv_net = tf.reshape(x, shape=[-1, 50, 50, 1]) # add channel dimensions
 
-#
-def maxpool2d(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME') #pool 2 pixels at a time
+    #used for scoping layers arguments
+    with slim.arg_scope([slim.conv2d],
+            padding = "SAME",
+            activation_fn = tf.nn.relu,
+            stride = 1,
+            weights_initializer = tf.truncated_normal_initializer(stddev=0.01),
+            weights_regularizer = slim.l2_regularizer(0.0005),
+            normalizer_fn = slim.batch_norm,
+            normalizer_params = {'scale' : True,'trainable' : False, 'is_training' : is_training }):
+        
+        conv_net = slim.conv2d(conv_net, 32, 3)
+        conv_net = slim.conv2d(conv_net, 64, 3)
+        conv_net = slim.conv2d(conv_net, 64, 3)
+        conv_net = slim.max_pool2d(conv_net, 3, stride = 1 )
+        conv_net = slim.conv2d(conv_net, 96, 3)
+        conv_net = slim.conv2d(conv_net, 96, 3)
+        conv_net = slim.max_pool2d(conv_net, 2, stride = 2)
+        
+        conv_net = slim.conv2d(conv_net, 128, 3)
+        conv_net = slim.conv2d(conv_net, 128, 3)
+        conv_net = slim.max_pool2d(conv_net, 2, stride = 2)
 
+        conv_net = slim.conv2d(conv_net, 128, 3)
+        conv_net = slim.conv2d(conv_net, 128, 3)
+        conv_net = slim.max_pool2d(conv_net, 2, stride = 2)
 
-########################################## NETWORK DEFINITION ################################################
-'''
-This cell contains a function that is used define the weights and biases of each layer in the
-network. It is called by the train_network function. It also lays out the
-structure of the network that goes as follows:
-conv1 -> maxpooling -> conv2 -> maxpooling - > conv3 -> fully connected layer (with dropout) -> output layer
+        conv_net = slim.conv2d(conv_net, 128, 3)
+        conv_net = slim.max_pool2d(conv_net, 2, stride = 1)
+        
+        conv_net = slim.dropout(conv_net, keep_prob = keep_rate, is_training = is_training )
 
-'''
-########################################## NETWORK DEFINITION ################################################
-'''
-This cell contains a function that is used define the weights and biases of each layer in the
-network. It is called by the train_network function. It also lays out the
-structure of the network that goes as follows:
-conv1 -> maxpooling -> conv2 -> maxpooling - > conv3 -> fully connected layer (with dropout) -> output layer
+    # Fully Connect Layer
+    with slim.arg_scope([slim.fully_connected ],weights_regularizer = slim.l2_regularizer(0.0005)):
+        conv_net = slim.flatten(conv_net)
+        output = slim.fully_connected(conv_net, num_class, activation_fn = None)
+        prediction = tf.nn.softmax(output)
 
-'''
-
-# Define the weights and biases as dictionaries and
-# define structure of the network
-def convolutional_network(x):
-    weights = {
-        'weights_conv1' : tf.Variable(tf.random_normal([5,5,1,64])),
-        'weights_conv2' : tf.Variable(tf.random_normal([5,5,64,128])),
-        'weights_conv3' : tf.Variable(tf.random_normal([5,5,128,256])),
-        'weights_fully_con' : tf.Variable(tf.random_normal([7*7*256,4096])),
-        'weights_out' : tf.Variable(tf.random_normal([4096, num_class]))
-    }
-
-    biases = {
-        'bias_conv1' : tf.Variable(tf.random_normal([64])),
-        'bias_conv2' : tf.Variable(tf.random_normal([128])),
-        'bias_conv3' : tf.Variable(tf.random_normal([256])),
-        'bias_fully_con' : tf.Variable(tf.random_normal([4096])),
-        'bias_out' : tf.Variable(tf.random_normal([num_class])) #CHANGE THIS IF NO USE
-    }
-
-    x = tf.reshape(x, shape=[-1, 50, 50, 1])
-
-    # 3 convolutional and 3 max pooling layers
-    conv1 = tf.nn.relu(conv2d(x, weights['weights_conv1']) + biases['bias_conv1'])
-    conv1 = maxpool2d(conv1)
-
-    conv2 = tf.nn.relu(conv2d(conv1, weights['weights_conv2']) + biases['bias_conv2'])
-    conv2 = maxpool2d(conv2)
-
-    conv3 = tf.nn.relu(conv2d(conv2, weights['weights_conv3']) + biases['bias_conv3'])
-    conv3 = maxpool2d(conv3)
-
-    # The fully connected layer
-    fully_con = tf.reshape(conv3, [-1, 7*7*256])
-    fully_con = tf.nn.relu(tf.matmul(fully_con, weights['weights_fully_con']) + biases['bias_fully_con'])
+        return output, prediction
     
-    #Apply dropout - 80% of the neurons are kept
-    fully_con = tf.nn.dropout(fully_con, keep_rate) # Apply dropout
+print('Network defined...')
 
-    output = tf.matmul(fully_con, weights['weights_out']) + biases['bias_out']
-    return output
 
-print('network defined')
-######################################## BATCHING ###################################################
+# In[12]:
+
 '''
-This cell is for segmenting the training data in to batches to relieve the GPU of being overloaded
-with data.
-EACH BATCH: 100 values (images data or label)
-EACH CLASS: 14 batch
-TOTAL BATCHES: 84 (14 x 6 classes = 84)
-
-FOR EXAMPLE
-BATCH_LABELS = [[0,0,0,0,0 ... 0], [0,0,0,0 .... 0]  ... 14 batches per class ... [1,1,1,1,1 .. 1], [1,1,1,1 ..] ...[...,5,5]] 
+Shuffle the batches on the fly.
 
 '''
 
-num_images = len(images)
-num_labels = len(labels)
-
-BATCHES_IMAGES = []
-BATCHES_LABELS = []
-
-batch_start= 0
-batch_end = 100
-#Batch the 8400 images into batchs of size 100
-for i in range(int(num_images/batch_size)):
-    temp_batch = images[batch_start:batch_end]
-    BATCHES_IMAGES.append(temp_batch)
-    batch_start = batch_start + 100
-    batch_end = batch_end + 100
-
-batch_start= 0
-batch_end = 100
-# batch the 8400 Label into 84 batchs of 100
-for i in range(int(num_labels/batch_size)):
-    temp_batch = labels[batch_start:batch_end]
-    BATCHES_LABELS.append(temp_batch)
-    batch_start = batch_start + 100
-    batch_end = batch_end + 100
-
-print('NUM BATCH IMAGES : ', len(BATCHES_IMAGES))
-print('NUM BATCHES LABELS : ', len(BATCHES_LABELS))
+def randomize(batch_x, batch_y):
+    batch_x, batch_y = shuffle(batch_x, batch_y)
+    return batch_x, batch_y
 
 
-######################################## TENSORFLOW SESSION ###################################################
-'''
-This cell contains a function that runs the tensorflow session, it is called with the x placeholders.
-The session is ran by first initializing all the tensorflow variables, then iterated through
-the number of epochs and feed the image data and labels using feed_dict.
-The loss/cost and accuracy is evaluated and printed to the console.
+# In[4]:
 
-'''
+######################################### NETWORK TRAINING #################################################
 '''
 This cell contains a function that runs the tensorflow session, it is called with the x placeholders.
 The session is ran by first initializing all the tensorflow variables, then iterated through
@@ -240,41 +271,105 @@ The loss/cost and accuracy is evaluated and printed to the console.
 '''
 
 def train_network(x):
-    pred = convolutional_network(x)
-    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = pred))
-    train_op = tf.train.AdamOptimizer(learning_rate=0.01).minimize(loss)
+    output, prediction = convolutional_network(x, is_training)
+    
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = output))
+    total_losses = tf.losses.get_total_loss( add_regularization_losses=True ) + loss
+    
+    update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS) # needed for batch normalization
+    with tf.control_dependencies(update_ops):
+        train_op = tf.train.AdamOptimizer( learning_rate=0.002 ).minimize(total_losses)
+    
+    correct = tf.equal(tf.argmax(prediction,1),  tf.argmax(y, 1))
+    acc = tf.reduce_mean(tf.cast(correct, 'float'))
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer()) # Initialize all the variables
         saver = tf.train.Saver()
-    
+
         time_full_start = time.clock()
+        
         print("RUNNING SESSION...")
         for epoch in range(num_epochs):
-            
             train_batch_x = []
             train_batch_y = []
-            epoch_loss = 0
+            epoch_loss= 0
+            epoch_total_loss = 0
+            accuracy = 0
             time_epoch_start = time.clock()
-            for i in range(0, 84):
-                train_batch_x = BATCHES_IMAGES[i]
-                train_batch_y = BATCHES_LABELS[i]    
-                op , loss_value = sess.run([train_op, loss], feed_dict={x: train_batch_x, y: train_batch_y})
+            i = 0
+            number_of_batches = 0
+            
+            #For all images in the DS, batch into sizes of 100
+            while i < len(images):
+                start = i
+                end = i + batch_size
+                train_batch_x = images[start:end]
+                train_batch_y = labels[start:end]
+                
+                #Randomize the batches even more
+                train_batch_x, train_batch_y = randomize(train_batch_x, train_batch_y)
+                
+                #Feed batches into tensorflow
+                op, ac, loss_value, total_loss_value = sess.run([train_op, acc, loss, total_losses],feed_dict={x: train_batch_x,
+                                                                    y: train_batch_y, is_training : True})
                 epoch_loss += loss_value
-                print('batch ', i)
-            print('Epoch : ', epoch+1, ' of ', num_epochs, ' - Loss for epoch: ', epoch_loss)
+                epoch_total_loss += total_loss_value
+                accuracy += ac
+                i += batch_size
+                number_of_batches += 1
+            
+            accuracy /= number_of_batches
+            
+            print('Epoch:', epoch+1, 'total loss: ', epoch_total_loss  ,' loss: ', epoch_loss ,' acc: {: %}'.format(accuracy))
             
             time_epoch_end = time.clock()
             print('Time elapse: ', time_epoch_end - time_epoch_start)
 
         time_full_end = time.clock()
         print('Full time elapse:', time_full_end - time_full_start)
-        
-        correct = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
-        acc = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', acc)
 
-        save_path = saver.save(sess, MODEL_PATH)
-        print("Model saved in file: " , save_path)
-############################################################################################################
+        if epoch_loss < 100:
+            save_path = saver.save(sess, MODEL_PATH)
+            print("Model saved in file: " , save_path)
+
+
+        # Evaluate on unseen test data
+        print('Accuracy:', acc.eval({x: test_images, y: test_labels, is_training : False }))
+
+
+# In[14]:
+
 train_network(x)
+
+
+# In[ ]:
+
+######################################### NETWORK TESTING #################################################
+'''
+Simple  cell for loading the model and testing the labels predicted for a range of
+images
+'''
+def test(x):
+    output, prediction = convolutional_network_v2(x, False )
+#     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels = y, logits = output))
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.import_meta_graph('/output/trained_model.ckpt.meta')
+        saver.restore(sess, '/output/trained_model.ckpt' )
+        print('session restored...')
+
+        pred_ = tf.nn.softmax(output)
+
+        predicted = sess.run(pred_, feed_dict={x: test_images[400:410]})[0]
+        
+        print('Actual Labels for ten images\n', test_labels[400:410])
+        print('\nPredicited Labels for ten images\n', predicted[400:410])
+test(x)
+
+
+# In[ ]:
+
+
+
